3bc1f4a7e42707c0fdb4beb3c16a0f1be7f5e54f
KRGAMING2005 <jagarblixt@gmail.com>
2024-05-14 15:57:12 +0200

    Added basic editor page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+
+    import type monaco from 'monaco-editor';
+    import { onMount } from "svelte";
+    
+    import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+    import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+    import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+    import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+    import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+    let divEl: HTMLDivElement;
+    let editor: monaco.editor.IStandaloneCodeEditor;
+    let Manoco;
+
+    onMount(async () => {
+        self.MonacoEnvironment = {
+            getWorker: function (_moduleId: any, label: string) {
+                if (label === 'json') {
+                    return new jsonWorker();
+                }
+                if (label === 'css' || label === 'scss' || label === 'less') {
+                    return new cssWorker();
+                }
+                if (label === 'html' || label === 'handlebars' || label === 'razor') {
+                    return new htmlWorker();
+                }
+                if (label === 'typescript' || label === 'javascript') {
+                    return new tsWorker();
+                }
+                return new editorWorker();
+            }
+        };
+
+        Manoco = await import('monaco-editor');
+        editor = Manoco.editor.create(divEl, {
+            value: ['function helloWorld() {', '\tconsole.log("Hello From Monaco");', '};', '', 'helloWorld();'].join("\n"),
+            language: 'typescript',
+            theme: 'vs-dark'
+        });
+
+        return (): void => {
+            editor.dispose();
+        }
+    });
+
+
+
+</script>
+
+<div bind:this={divEl} id="editor"></div>
+
+<style>
+    :global(*) {
+        padding: 0;
+        margin: 0;
+    }
+    #editor {
+        height: 100dvh;
+    }
+</style>
